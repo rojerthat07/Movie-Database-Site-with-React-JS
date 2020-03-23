ced4e3b3736537eca112cb2aade2e0023a971a3d
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-03-23 09:12:41 +0800

    Navigation completion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+//Styles
+import './main.scss'
+//Components
+import Navigation from './components/Navigation'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Jer Carlo</h1>
+        <Navigation />
+        <h1>App.js</h1>
       </div>
     )
   }
---
+++ src/components/Navigation.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import brand from '../images/tmdb.png'
+
+export class Navigation extends Component {
+    render() {
+        return (
+            <div className="navigation">
+
+            <div className="navigation__brand">
+              <img className="navigation__image" src={brand} alt=""/>  <p>Movie Database</p>
+
+            </div>
+
+
+                <div className="navigation__list">
+                    <div className="navigation__item">
+                       <a href="#" className="navigation__link">HOME</a>
+                    </div>
+                    <div className="navigation__item">
+                       <a href="#" className="navigation__link">ABOUT</a>
+                    </div>
+                    <div className="navigation__item">
+                       <a href="#" className="navigation__link">FACTS</a>
+                    </div>
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default Navigation
