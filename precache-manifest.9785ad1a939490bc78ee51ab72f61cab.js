cb07cce4f74808c0fe1d408df81bf29f4cf192bf
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-03-31 10:33:00 +0800

    Updates

---
+++ precache-manifest.9785ad1a939490bc78ee51ab72f61cab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bd28a859556d52c14ca44d7fce4f98",
+    "url": "/Movie-Database-Site-with-React-JS/index.html"
+  },
+  {
+    "revision": "1cf4d16d0271f936dab8",
+    "url": "/Movie-Database-Site-with-React-JS/static/css/main.fb949359.chunk.css"
+  },
+  {
+    "revision": "d2b5644aeec71410a7e9",
+    "url": "/Movie-Database-Site-with-React-JS/static/js/2.a1c16943.chunk.js"
+  },
+  {
+    "revision": "e9a45d9472c063c8a1c023664dfc3c9f",
+    "url": "/Movie-Database-Site-with-React-JS/static/js/2.a1c16943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf4d16d0271f936dab8",
+    "url": "/Movie-Database-Site-with-React-JS/static/js/main.52705e4f.chunk.js"
+  },
+  {
+    "revision": "c38748415441814f6521",
+    "url": "/Movie-Database-Site-with-React-JS/static/js/runtime-main.a1b5a495.js"
+  },
+  {
+    "revision": "5d17c4aaf65f2e7b521a6131924677fe",
+    "url": "/Movie-Database-Site-with-React-JS/static/media/headerbg.5d17c4aa.jpg"
+  }
+]);
