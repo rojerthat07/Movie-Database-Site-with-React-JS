fa933f6b77ac58a5cdf0db166a417dfd4e46a78f
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-03-31 09:54:07 +0800

    header, changing width of movie rows and footer font size

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,13 @@ import siteLogo from '../images/tmdb.png'
 function About() {
     return (
         <div className="about">
-            <h1>ABOUT</h1>
-            <h3>mr. MovieDB Theater</h3>
+            <div className="about__heading">ABOUT</div>
+            <div className="about__heading2">mr. MovieDB Theater</div>
+    
+            <div className="about__text">This site is built with React JS and using MovieDB API. This project is part of the portfolio of Jer Carlo D. Catallo.</div>
+          
             <img src={siteLogo} alt=""/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aut eos placeat iure voluptas eaque asperiores aspernatur nam. Voluptatem dignissimos aliquid error nam rerum, ullam, in fuga inventore ex, quisquam optio repudiandae recusandae. Voluptas libero incidunt quasi ad, in, earum ipsa mollitia deserunt doloremque sint tempora rem perspiciatis quaerat laborum.</p>
+           
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ function Header() {
         <div className="header" style={headerStyle}>
             <div className="header__information">
             <div className="header__title">World's Largest Movie Database Site</div>
-            <div className="header__text">Made by Jer Carlo Catallo</div>
+            <div className="header__text">
+Millions of movies, TV shows and people to discover. Explore now.</div>
             </div>
            
         </div>
