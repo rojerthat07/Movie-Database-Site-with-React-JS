31c3511fb6fdc85e753f6e8007fc19131644f5b5
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-07-22 09:49:10 +0800

    adding aditional info(vote,release,language,popularity), chaging color pallete

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -21,14 +21,50 @@ class MovieRow extends Component {
             </div>
 
             <div className="movies__data">
+
+              <div className="movies__description">
               <div className="movies__title" title={this.props.movie.title}>{this.props.movie.title}</div>
               
               <div className="movies__overview">
                 {this.props.movie.overview}
                 </div> 
-           
+              <div className="movies__stats">
+                <div className="movies__stats-data">
+                Vote average:  
+              
+              <span>
+              {this.props.movie.vote_average}
+              </span>
+                </div>
+                <div className="movies__stats-data">
+                Release Date:  
+              
+              <span>
+              {this.props.movie.release_date}
+              </span>
+                </div>
+                <div className="movies__stats-data">
+                Language:  
+              
+              <span>
+                {this.props.movie.original_language}
+              </span>
+                </div>
+                <div className="movies__stats-data">
+                Popularity:  
+                <span>
+                {this.props.movie.popularity}
+                </span>
+              
+                </div>
+                
+              </div>
+        
               </div>
               
+           
+              </div>
+
           </div>
  
       </div>
