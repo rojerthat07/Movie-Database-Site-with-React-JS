e60447f278c01df0d51ba5f7ae7637d631fce84b
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-07-27 08:17:16 +0800

    styling search box

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ class App extends Component {
 
   render() {
     const jer ={
-      background:'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('+this.state.backdrop+')'
+      background:'linear-gradient(rgba(0,0,0,0.9),rgba(0,0,0,0.3),rgba(0,0,0,0.3),rgba(0,0,0,0.9)),url('+this.state.backdrop+')'
     }
 
     return (
