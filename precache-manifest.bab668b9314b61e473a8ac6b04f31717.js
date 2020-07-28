5dfc1afffbac8485e0963f49cac851491e015d8f
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-07-28 08:46:46 +0800

    Updates

--- precache-manifest.bab668b9314b61e473a8ac6b04f31717.js
+++ precache-manifest.bab668b9314b61e473a8ac6b04f31717.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5275212f324cf04a52edcb4a578ac788",
+    "revision": "ea6aca30ea7c434f7c1229e3d176e5de",
     "url": "/Movie-Database-Site-with-React-JS/index.html"
   },
   {
-    "revision": "c1730ce811489b44775b",
+    "revision": "80fa56bfc069a8131a78",
     "url": "/Movie-Database-Site-with-React-JS/static/css/main.693fb3b4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Movie-Database-Site-with-React-JS/static/js/2.9d402813.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c1730ce811489b44775b",
-    "url": "/Movie-Database-Site-with-React-JS/static/js/main.4a2cd39c.chunk.js"
+    "revision": "80fa56bfc069a8131a78",
+    "url": "/Movie-Database-Site-with-React-JS/static/js/main.bbc1285f.chunk.js"
   },
   {
     "revision": "c38748415441814f6521",
