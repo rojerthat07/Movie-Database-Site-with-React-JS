4ffc05e70ee082e258c2b6bb857cc32d098d9dde
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-07-28 08:48:24 +0800

    Updates

---
+++ precache-manifest.530daf130620b73bfbd99a8fa41f718c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a854f4cd94f975d4b7c594596c2155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b7e620bc8bfb3b0729f",
+    "url": "/static/css/main.693fb3b4.chunk.css"
+  },
+  {
+    "revision": "a940e7e3c76afbb52859",
+    "url": "/static/js/2.9d402813.chunk.js"
+  },
+  {
+    "revision": "e9a45d9472c063c8a1c023664dfc3c9f",
+    "url": "/static/js/2.9d402813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b7e620bc8bfb3b0729f",
+    "url": "/static/js/main.dee6ec1f.chunk.js"
+  },
+  {
+    "revision": "ed93c66efd4f82af67f3",
+    "url": "/static/js/runtime-main.ae810e02.js"
+  }
+]);
